60209f06e9a8ea2cbfada6f02e485a4016d034f3
Javier Gómez <f.gomez@astoundcommerce.com>
2021-02-17 14:28:41 -0500

    archivos iniciales

---
+++ index.js
@@ -0,0 +1,19 @@
+var name = 'Laura';
+console.log(name);
+
+name = 'Javier';
+
+
+
+
+console.log(name);
+
+function suma(suma1, sumand2) {
+  return suma1 + sumand2;
+}
+console.log(suma(1,2));
+
+console.log(document);
+
+var test = document.getElementById('test');
+console.log(test);
